870a26cb1dd853add0d2f1c5642d038c4b58ea4f
Chunting Liu <c6869ppm@gmail.com>
2017-06-05 10:20:35 +0800

    Update TamagotchiCode.ino

--- Code/TamagotchiCode.ino
+++ Code/TamagotchiCode.ino
@@ -7,8 +7,13 @@
 #define PIN_DC    5
 #define PIN_SDIN  4
 #define PIN_SCLK  3
+#define buttonSelect 13
 
 byte count = 0;
+byte xChicken = 42;
+bool chickenFlag = 0;
+bool select_Now = 0;
+bool select_Before = 0;
 
 Adafruit_PCD8544 display = Adafruit_PCD8544(PIN_SCLK, PIN_SDIN, PIN_DC, PIN_SCE, PIN_RESET);
 
@@ -40,12 +45,41 @@ static const unsigned char PROGMEM Chicken[] =
   B00000011, B00000000, B00110000,
   B00000011, B00000000, B00110000,
   B00000011, B00000000, B00110000
-};
+  };
+
+static const unsigned char PROGMEM ChickenReverse[] =
+{
+  B00000001 ,B00010001 ,B00000000,
+  B00000000 ,B10010010 ,B00000000,
+  B00000000 ,B01010100 ,B00000000,
+  B00001111 ,B11111111 ,B11110000,
+  B00010000 ,B00000000 ,B00001000,
+  B00010000 ,B00000000 ,B00001000,
+  B00010000 ,B00000000 ,B11101100,
+  B00010000 ,B00000000 ,B11101010,
+  B00010000 ,B00000000 ,B11101001,
+  B00010000 ,B00000000 ,B00001010,
+  B00010000 ,B00000000 ,B00001100,
+  B00010000 ,B00000000 ,B00001000,
+  B00010000 ,B00000000 ,B00001000,
+  B11100000 ,B00000000 ,B00001000,
+  B10000000 ,B00000000 ,B00001000,
+  B10000000 ,B00000000 ,B00001000,
+  B10000000 ,B00000000 ,B00001000,
+  B10000000 ,B00000000 ,B00001000,
+  B10000000 ,B00000000 ,B00001000,
+  B10000000 ,B00000000 ,B00001000,
+  B01100000 ,B00000000 ,B00001000,
+  B00011111 ,B11111111 ,B11110000,
+  B00001100 ,B00000000 ,B11000000,
+  B00001100 ,B00000000 ,B11000000,
+  B00001100 ,B00000000 ,B11000000
+  };
 
 void setup() {
 
   Serial.begin(9600);
-
+  pinMode(buttonSelect, INPUT);
   display.begin();
   //display.setContrast(60);
 
@@ -59,25 +93,53 @@ void setup() {
   display.drawLine(28, 0, 28, 15, BLACK);
   display.drawLine(56, 0, 56, 15, BLACK);
 
-  //小雞
-  display.drawBitmap(20, 20, Chicken, 24, 24, BLACK);
-
   display.display();
 
 }
 
-void kClear(byte x1, byte y1, byte x2, byte y2, bool color) { //x1,y1等於起始座標 x2,y2等於終點座標 color為顏色 0=白 1=黑
-  if (color == 0)
-    for (byte j = y1; j < y2; j++)
-      for (byte i = x1; i < x2; i++)
-        display.drawPixel(j, i, WHITE);
-  else if (color == 1)
-    for (byte j = y1; j < y2; j++)
-      for (byte i = x1; i < x2; i++)
-        display.drawPixel(j, i, BLACK);
-}
+
 
 void loop() {
+  select_Now = digitalRead(buttonSelect);
+  /*
+  以兩個變數去判斷是否按下按鈕 
+  當Now=1,Before=1則進入選項內，其餘狀況則不做任何反映 || 
+  需要此功能是因為Button按下後會回傳一個bool型態，會是永久1或0
+  */
+  if(select_Now == 1 && select_Before == 0){
+    count++;
+    if(count == 4)
+      count = 0;
+  }
+  select_Before = select_Now;
+  
+  chickenMove();
+  UI();
+  delay(100);
+  display.display();
+
+}
+
+void chickenMove(){
+  kClear(xChicken - 1, 20, xChicken + 25, 44, WHITE);
+  //小雞
+  
+  if (chickenFlag){ //倒走
+    display.drawBitmap(xChicken, 20, ChickenReverse, 24, 24, BLACK);
+    xChicken++;
+  }
+  else{             //正走
+    display.drawBitmap(xChicken, 20, Chicken, 24, 24, BLACK);
+    xChicken--;
+  }
+  if(xChicken == 1)
+    chickenFlag = 1;
+  else if(xChicken == 59)
+    chickenFlag = 0;
+  }
+
+void UI(){
+  
   if (count == 0) { //未選擇
 
     //三角形，大便
@@ -100,11 +162,11 @@ void loop() {
     display.drawLine(74, 1, 70, 5, BLACK); //ba
 
   }
-  if (count == 1) { //選三角形
+  else if (count == 1) { //選三角形
 
     //三角形，大便(反白)
-    kClear(0, 0, 27 , 14, 1);
-    display.drawTriangle(14, 3 , 7, 12, 21, 12, WHITE);
+    kClear(0, 0, 28, 15, 1);
+    display.drawTriangle(14, 3, 7, 12, 21, 12, WHITE);
 
     //圓圈+點，吃飯(餅乾)
     display.drawCircle(42, 7, 5, BLACK);
@@ -121,16 +183,14 @@ void loop() {
     display.drawLine(70, 13, 78, 5, BLACK); //dc
     display.drawLine(78, 5, 74, 1, BLACK); //cb
     display.drawLine(74, 1, 70, 5, BLACK); //ba
-
-
   }
-  if (count == 2) { //選餅乾
+  else if (count == 2) { //選餅乾
 
     //三角形，大便
-    display.drawTriangle(14, 3 , 7, 12, 21, 12, BLACK);
+    display.drawTriangle(14, 3, 7, 12, 21, 12, BLACK);
 
     //圓圈+點，吃飯(餅乾)(反白)
-    kClear(29, 0, 55 , 14, 1);
+    kClear(28, 0, 56 , 15, 1);
     display.drawCircle(42, 7, 5, WHITE);
     display.drawPixel(42, 7, WHITE);
     display.drawPixel(40, 5, WHITE);
@@ -146,12 +206,11 @@ void loop() {
     display.drawLine(78, 5, 74, 1, BLACK); //cb
     display.drawLine(74, 1, 70, 5, BLACK); //ba
 
-
   }
-  if (count == 3) { //選愛心
+  else if (count == 3) { //選愛心
 
     //三角形，大便
-    display.drawTriangle(14, 3 , 7, 12, 21, 12, BLACK);
+    display.drawTriangle(14, 3, 7, 12, 21, 12, BLACK);
 
     //圓圈+點，吃飯(餅乾)
     display.drawCircle(42, 7, 5, BLACK);
@@ -162,7 +221,7 @@ void loop() {
     display.drawPixel(44, 9, BLACK);
 
     //愛心(反白)
-    kClear(57, 0, 84 , 14, 1);
+    kClear(57, 0, 84, 14, 1);
     display.drawLine(70, 5, 66, 1, WHITE); //af
     display.drawLine(66, 1, 62, 5, WHITE); //fe
     display.drawLine(62, 5, 70, 13, WHITE); //ed
@@ -171,4 +230,15 @@ void loop() {
     display.drawLine(74, 1, 70, 5, WHITE); //ba
 
   }
+}
+
+void kClear(byte x1, byte y1, byte x2, byte y2, bool color) { //x1,y1等於起始座標 x2,y2等於終點座標 color為顏色 0=白 1=黑
+  if (color == 0)
+    for (byte j = y1; j < y2; j++)
+      for (byte i = x1; i < x2; i++)
+        display.drawPixel(i, j, WHITE);
+  else if (color == 1)
+    for (byte j = y1; j < y2; j++)
+      for (byte i = x1; i < x2; i++)
+        display.drawPixel(i, j, BLACK);
 }
